cfa37f2b810f3911e7cf0c80833a41d254594f9e
ashoshari <aasemshoshari2040@gmail.com>
2024-05-15 16:10:01 +0300

    asd

--- src/components/componentContainer/ComponentContainer.jsx
+++ src/components/componentContainer/ComponentContainer.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useParams } from "react-router-dom";
 import ProjectsComponent from "./projects/ProjectsComponent";
 import RequestVacationForm from "./formContainer/RequestVacationForm";
@@ -11,49 +10,49 @@ function ComponentContainer() {
 
   if (component === "employees") {
     return (
-      <div className="componentContainer">
+      <div className="componentContainer px-3">
         <TableContainer table={"Employee"} />
       </div>
     );
   } else if (component === "projects") {
     return (
-      <div className="componentContainer">
+      <div className="componentContainer px-3">
         <ProjectsComponent />
       </div>
     );
   } else if (component === "request-vacation") {
     return (
-      <div className="componentContainer">
+      <div className="componentContainer px-3">
         <RequestVacationForm />
       </div>
     );
   } else if (component === "vacations") {
     return (
-      <div className="componentContainer">
+      <div className="componentContainer px-3">
         <TableContainer table={"Vacation"} />
       </div>
     );
   } else if (component === "salaries") {
     return (
-      <div className="componentContainer">
+      <div className="componentContainer px-3">
         <TableContainer table={"Salary"} />
       </div>
     );
   } else if (component === "reports") {
     return (
-      <div className="componentContainer">
+      <div className="componentContainer px-3">
         <ReportsComponent />
       </div>
     );
   } else if (component === "careers") {
     return (
-      <div className="componentContainer">
+      <div className="componentContainer px-3">
         <CareerForm />
       </div>
     );
   } else if (component === "contact") {
     return (
-      <div className="componentContainer">
+      <div className="componentContainer px-3">
         <ContactUsComponent />
       </div>
     );
